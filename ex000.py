e686949fb4bdb7e60cfa343c34ae1a56e7453e68
Felipe Dantas do Nascimento <felipedantas23@outlook.com>
2024-05-17 09:43:10 -0300

    Add files via upload

---
+++ ex000.py
@@ -0,0 +1,8 @@
+#Formas de print
+youtuber = "Felipe Dantas"
+print("se inscreva em " + youtuber)
+print("se inscreva em {}".format(youtuber))
+print(f"se inscreva em {youtuber}")
+
+
+
---
+++ ex005.py
@@ -0,0 +1,10 @@
+
+adj = input("Adjetivo: ")
+verb1 = input("Verbo: ")
+verb2 = input("Verbo: ")
+famous_person = input("Famoso: ")
+madlib = f"Programação é tão {adj}! Isso me faz ficar muito animado por que \
+    Eu amo {verb1}. Se manter hidratado e {verb2} e você será {famous_person}!"
+
+
+print(madlib) 
